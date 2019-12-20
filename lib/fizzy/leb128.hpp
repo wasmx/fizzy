50139c51a4be34464a252c7058dd6e655949aa59
Andrei Maiboroda <andrei@ethereum.org>
2019-12-20 13:30:51 +0100

    LEB128 unsigned decoder implementation

--- lib/fizzy/leb128.hpp
+++ lib/fizzy/leb128.hpp
@@ -2,8 +2,18 @@
 
 namespace fizzy
 {
-inline constexpr uint64_t leb128_decode(const uint8_t*) noexcept
+inline constexpr uint64_t leb128u_decode(const uint8_t* input) noexcept
 {
-    return 0;
+    uint64_t result = 0;
+    uint8_t result_shift = 0;
+
+    for (auto byte = input;; ++byte, result_shift += 7)
+    {
+        result |= static_cast<uint64_t>(*byte & 0x7F) << result_shift;
+        if ((*byte & 0x80) == 0)
+            break;
+    }
+
+    return result;
 }
 }  // namespace fizzy
--- test/unittests/leb128_test.cpp
+++ test/unittests/leb128_test.cpp
@@ -1,14 +1,17 @@
 #include "leb128.hpp"
 #include <gtest/gtest.h>
 
-TEST(leb128, DISABLED_decode)
+TEST(leb128, decode_unsigned)
 {
     uint8_t encoded_1 = 1;
-    EXPECT_EQ(fizzy::leb128_decode(&encoded_1), 1);
-}
+    EXPECT_EQ(fizzy::leb128u_decode(&encoded_1), 1);
 
-TEST(leb128, dummy)
-{
-    auto s = std::string{"hello"};
-    EXPECT_FALSE(s.empty());
+    uint8_t encoded_624485[] = {0xe5, 0x8e, 0x26};
+    EXPECT_EQ(fizzy::leb128u_decode(encoded_624485), 624485);
+
+    uint8_t encoded_bigger_than_int32[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00};
+    EXPECT_EQ(fizzy::leb128u_decode(encoded_bigger_than_int32), 562949953421311);
+
+    uint8_t encoded_max[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x01};
+    EXPECT_EQ(fizzy::leb128u_decode(encoded_max), std::numeric_limits<uint64_t>::max());
 }
