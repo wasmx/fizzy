6824340c2368c9812ad26d81c0aca8f5449dc117
Paweł Bylica <chfast@gmail.com>
2019-12-20 16:03:35 +0100

    Setup CMake project with unit tests

---
+++ lib/fizzy/dummy.cpp
@@ -0,0 +1,1 @@
+#include "leb128.hpp"
---
+++ lib/fizzy/leb128.hpp
@@ -0,0 +1,9 @@
+#include <cstdint>
+
+namespace fizzy
+{
+inline constexpr uint64_t leb128_decode(const uint8_t*) noexcept
+{
+    return 0;
+}
+}  // namespace fizzy
