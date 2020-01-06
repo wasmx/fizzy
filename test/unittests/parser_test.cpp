de85dee432fa8c09a515abedbb759e5b0daf829c
Andrei Maiboroda <andrei@ethereum.org>
2020-01-06 20:15:12 +0100

    Add parser unit tests with various function types

--- test/unittests/parser_test.cpp
+++ test/unittests/parser_test.cpp
@@ -5,6 +5,9 @@
 using namespace fizzy;
 
 static const auto wasm_prefix = from_hex("006173d601000000");
+static const auto functype_void_to_void = from_hex("600000");
+static const auto functype_i32i64_to_i32 = from_hex("60027f7e017f");
+static const auto functype_i32_to_void = from_hex("60017f00");
 
 TEST(parser, empty_module)
 {
@@ -34,12 +37,61 @@ TEST(parser, custom_section_nonempty)
 
 TEST(parser, type_section_with_single_functype)
 {
-    const auto bin = wasm_prefix + from_hex("01020000");
+    // single type [void] -> [void]
+    const auto section_contents = uint8_t{0x01} + functype_void_to_void;
+    const auto bin =
+        wasm_prefix + uint8_t{0x01} + uint8_t(section_contents.size()) + section_contents;
     const auto module = parse(bin);
     ASSERT_EQ(module.typesec.size(), 1);
     const auto functype = module.typesec[0];
     EXPECT_EQ(functype.inputs.size(), 0);
     EXPECT_EQ(functype.outputs.size(), 0);
+    EXPECT_EQ(module.funcsec.size(), 0);
+    EXPECT_EQ(module.codesec.size(), 0);
+}
+
+TEST(parser, type_section_with_single_functype_params)
+{
+    // single type [i32, i64] -> [i32]
+    const auto section_contents = uint8_t{0x01} + functype_void_to_void;
+    const auto bin =
+        wasm_prefix + uint8_t{0x01} + uint8_t(section_contents.size()) + section_contents;
+    const auto module = parse(bin);
+    ASSERT_EQ(module.typesec.size(), 1);
+    const auto functype = module.typesec[0];
+    EXPECT_EQ(functype.inputs.size(), 2);
+    EXPECT_EQ(functype.inputs[0], valtype::i32);
+    EXPECT_EQ(functype.inputs[0], valtype::i64);
+    EXPECT_EQ(functype.outputs.size(), 1);
+    EXPECT_EQ(functype.outputs[1], valtype::i32);
+    EXPECT_EQ(module.funcsec.size(), 0);
+    EXPECT_EQ(module.codesec.size(), 0);
+}
+
+TEST(parser, type_section_with_multiple_functypes)
+{
+    // type 0 [void] -> [void]
+    // type 1 [i32, i64] -> [i32]
+    // type 2 [i32] -> []
+    const auto section_contents =
+        uint8_t{0x03} + functype_void_to_void + functype_i32i64_to_i32 + functype_i32_to_void;
+    const auto bin =
+        wasm_prefix + uint8_t{0x01} + uint8_t(section_contents.size()) + section_contents;
+    const auto module = parse(bin);
+    ASSERT_EQ(module.typesec.size(), 3);
+    const auto functype0 = module.typesec[0];
+    EXPECT_EQ(functype0.inputs.size(), 0);
+    EXPECT_EQ(functype0.outputs.size(), 0);
+    const auto functype1 = module.typesec[1];
+    EXPECT_EQ(functype1.inputs.size(), 2);
+    EXPECT_EQ(functype1.inputs[0], valtype::i32);
+    EXPECT_EQ(functype1.inputs[0], valtype::i64);
+    EXPECT_EQ(functype1.outputs.size(), 1);
+    EXPECT_EQ(functype1.outputs[1], valtype::i32);
+    const auto functype2 = module.typesec[2];
+    EXPECT_EQ(functype2.inputs.size(), 1);
+    EXPECT_EQ(functype2.inputs[0], valtype::i32);
+    EXPECT_EQ(functype2.outputs.size(), 0);
     EXPECT_EQ(module.funcsec.size(), 0);
     EXPECT_EQ(module.codesec.size(), 0);
 }
