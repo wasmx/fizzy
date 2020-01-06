b3aa9aa5308049cdd1058fe26ef3c26abe99d7ff
Andrei Maiboroda <andrei@ethereum.org>
2020-01-06 17:58:26 +0100

    Use bytes_view as input parameter instead of pointer + size

--- lib/fizzy/leb128.hpp
+++ lib/fizzy/leb128.hpp
@@ -1,14 +1,16 @@
+#include "types.hpp"
 #include <cstdint>
 #include <stdexcept>
 
 namespace fizzy
 {
-uint64_t leb128u_decode(const uint8_t* input, size_t size)
+uint64_t leb128u_decode(bytes_view input)
 {
     uint64_t result = 0;
     int result_shift = 0;
 
-    for (auto byte = input; byte != input + size && result_shift <= 63; ++byte, result_shift += 7)
+    for (auto byte = input.cbegin(); byte != input.cend() && result_shift <= 63;
+         ++byte, result_shift += 7)
     {
         result |= static_cast<uint64_t>(*byte & 0x7F) << result_shift;
         if ((*byte & 0x80) == 0)
--- test/unittests/leb128_test.cpp
+++ test/unittests/leb128_test.cpp
@@ -3,48 +3,48 @@
 
 TEST(leb128, decode_unsigned)
 {
-    uint8_t encoded_0 = 0;
-    EXPECT_EQ(fizzy::leb128u_decode(&encoded_0, 1), 0);
+    fizzy::bytes encoded_0{0};
+    EXPECT_EQ(fizzy::leb128u_decode(encoded_0), 0);
 
-    uint8_t encoded_0_leading_zeroes[] = {0x80, 0x80, 0x00};
-    EXPECT_EQ(fizzy::leb128u_decode(encoded_0_leading_zeroes, 3), 0);
+    fizzy::bytes encoded_0_leading_zeroes{0x80, 0x80, 0x00};
+    EXPECT_EQ(fizzy::leb128u_decode(encoded_0_leading_zeroes), 0);
 
-    uint8_t encoded_1 = 1;
-    EXPECT_EQ(fizzy::leb128u_decode(&encoded_1, 1), 1);
+    fizzy::bytes encoded_1{1};
+    EXPECT_EQ(fizzy::leb128u_decode(encoded_1), 1);
 
-    uint8_t encoded_1_leading_zeroes[] = {0x81, 0x80, 0x80, 0x00};
-    EXPECT_EQ(fizzy::leb128u_decode(encoded_1_leading_zeroes, 4), 1);
+    fizzy::bytes encoded_1_leading_zeroes{0x81, 0x80, 0x80, 0x00};
+    EXPECT_EQ(fizzy::leb128u_decode(encoded_1_leading_zeroes), 1);
 
-    uint8_t encoded_1_max_leading_zeroes[] = {0x81, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80};
-    EXPECT_THROW(fizzy::leb128u_decode(encoded_1_max_leading_zeroes, 9), std::runtime_error);
+    fizzy::bytes encoded_1_max_leading_zeroes{0x81, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80};
+    EXPECT_THROW(fizzy::leb128u_decode(encoded_1_max_leading_zeroes), std::runtime_error);
 
-    uint8_t encoded_624485[] = {0xe5, 0x8e, 0x26};
-    EXPECT_EQ(fizzy::leb128u_decode(encoded_624485, 3), 624485);
+    fizzy::bytes encoded_624485{0xe5, 0x8e, 0x26};
+    EXPECT_EQ(fizzy::leb128u_decode(encoded_624485), 624485);
 
-    uint8_t encoded_624485_leading_zeroes[] = {0xe5, 0x8e, 0xa6, 0x80, 0x80, 0x00};
-    EXPECT_EQ(fizzy::leb128u_decode(encoded_624485_leading_zeroes, 6), 624485);
+    fizzy::bytes encoded_624485_leading_zeroes{0xe5, 0x8e, 0xa6, 0x80, 0x80, 0x00};
+    EXPECT_EQ(fizzy::leb128u_decode(encoded_624485_leading_zeroes), 624485);
 
-    uint8_t encoded_bigger_than_int32[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00};
-    EXPECT_EQ(fizzy::leb128u_decode(encoded_bigger_than_int32, 8), 562949953421311);
+    fizzy::bytes encoded_bigger_than_int32{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00};
+    EXPECT_EQ(fizzy::leb128u_decode(encoded_bigger_than_int32), 562949953421311);
 
-    uint8_t encoded_bigger_than_int32_leading_zeroes[] = {
+    fizzy::bytes encoded_bigger_than_int32_leading_zeroes{
         0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x80, 0x80, 0x00};
-    EXPECT_EQ(fizzy::leb128u_decode(encoded_bigger_than_int32_leading_zeroes, 10), 562949953421311);
+    EXPECT_EQ(fizzy::leb128u_decode(encoded_bigger_than_int32_leading_zeroes), 562949953421311);
 
-    uint8_t encoded_max[] = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x01};
-    EXPECT_EQ(fizzy::leb128u_decode(encoded_max, 10), std::numeric_limits<uint64_t>::max());
+    fizzy::bytes encoded_max{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x01};
+    EXPECT_EQ(fizzy::leb128u_decode(encoded_max), std::numeric_limits<uint64_t>::max());
 }
 
 TEST(leb128, decode_unsigned_invalid)
 {
-    uint8_t encoded_624485_invalid[] = {0xe5, 0x8e, 0xa6};
-    EXPECT_THROW(fizzy::leb128u_decode(encoded_624485_invalid, 3), std::runtime_error);
+    fizzy::bytes encoded_624485_invalid{0xe5, 0x8e, 0xa6};
+    EXPECT_THROW(fizzy::leb128u_decode(encoded_624485_invalid), std::runtime_error);
 
-    uint8_t encoded_1_too_many_leading_zeroes[] = {
+    fizzy::bytes encoded_1_too_many_leading_zeroes{
         0x81, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80};
-    EXPECT_THROW(fizzy::leb128u_decode(encoded_1_too_many_leading_zeroes, 10), std::runtime_error);
+    EXPECT_THROW(fizzy::leb128u_decode(encoded_1_too_many_leading_zeroes), std::runtime_error);
 
-    uint8_t encoded_max_leading_zeroes[] = {
+    fizzy::bytes encoded_max_leading_zeroes{
         0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x81, 0x00};
-    EXPECT_THROW(fizzy::leb128u_decode(encoded_max_leading_zeroes, 11), std::runtime_error);
+    EXPECT_THROW(fizzy::leb128u_decode(encoded_max_leading_zeroes), std::runtime_error);
 }
