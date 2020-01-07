6e463c03d113bb58b40c5e857934df39ebf374b1
Andrei Maiboroda <andrei@ethereum.org>
2020-01-07 11:38:37 +0100

    Make LEB128 tests more table-driven; also check the returned pointer

--- test/unittests/leb128_test.cpp
+++ test/unittests/leb128_test.cpp
@@ -3,44 +3,30 @@
 
 TEST(leb128, decode_u64)
 {
-    fizzy::bytes encoded_0{0};
-    EXPECT_EQ(fizzy::leb128_decode_u64(encoded_0.data()).first, 0);
+    // clang-format off
+    std::vector<std::pair<fizzy::bytes, uint64_t>> testcases = {
+        {{0}, 0}, // 0
+        {{0x80, 0x80, 0x00}, 0}, // 0 with leading zeroes
+        {{1}, 1}, // 1
+        {{0x81, 0x80, 0x80, 0x00}, 1}, // 1 with leading zeroes
+        {{0x81, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x00}, 1}, // 1 with max leading zeroes
+        {{0xe5, 0x8e, 0x26}, 624485}, // 624485
+        {{0xe5, 0x8e, 0xa6, 0x80, 0x80, 0x00}, 624485}, // 624485 with leading zeroes
+        {{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00}, 562949953421311}, // bigger than int32
+        {{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x80, 0x80, 0x00}, 562949953421311}, // bigger than int32 with zeroes
+        {{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x01}, std::numeric_limits<uint64_t>::max()}, // max
+        // this is not really correct encoding, but is not rejected by our decoder
+        {{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7f}, std::numeric_limits<uint64_t>::max()},
+    };
+    // clang-format on
 
-    fizzy::bytes encoded_0_leading_zeroes{0x80, 0x80, 0x00};
-    EXPECT_EQ(fizzy::leb128_decode_u64(encoded_0_leading_zeroes.data()).first, 0);
-
-    fizzy::bytes encoded_1{1};
-    EXPECT_EQ(fizzy::leb128_decode_u64(encoded_1.data()).first, 1);
-
-    fizzy::bytes encoded_1_leading_zeroes{0x81, 0x80, 0x80, 0x00};
-    EXPECT_EQ(fizzy::leb128_decode_u64(encoded_1_leading_zeroes.data()).first, 1);
-
-    fizzy::bytes encoded_1_max_leading_zeroes{
-        0x81, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x00};
-    EXPECT_EQ(fizzy::leb128_decode_u64(encoded_1_max_leading_zeroes.data()).first, 1);
-
-    fizzy::bytes encoded_624485{0xe5, 0x8e, 0x26};
-    EXPECT_EQ(fizzy::leb128_decode_u64(encoded_624485.data()).first, 624485);
-
-    fizzy::bytes encoded_624485_leading_zeroes{0xe5, 0x8e, 0xa6, 0x80, 0x80, 0x00};
-    EXPECT_EQ(fizzy::leb128_decode_u64(encoded_624485_leading_zeroes.data()).first, 624485);
-
-    fizzy::bytes encoded_bigger_than_int32{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00};
-    EXPECT_EQ(fizzy::leb128_decode_u64(encoded_bigger_than_int32.data()).first, 562949953421311);
-
-    fizzy::bytes encoded_bigger_than_int32_leading_zeroes{
-        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x80, 0x80, 0x00};
-    EXPECT_EQ(fizzy::leb128_decode_u64(encoded_bigger_than_int32_leading_zeroes.data()).first,
-        562949953421311);
-
-    fizzy::bytes encoded_max{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x01};
-    EXPECT_EQ(
-        fizzy::leb128_decode_u64(encoded_max.data()).first, std::numeric_limits<uint64_t>::max());
-
-    // this is not really correct encoding, but is not rejected by our decoder
-    fizzy::bytes encoded_max2{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7f};
-    EXPECT_EQ(
-        fizzy::leb128_decode_u64(encoded_max2.data()).first, std::numeric_limits<uint64_t>::max());
+    for (auto const& testcase : testcases)
+    {
+        auto res = fizzy::leb128u_decode<uint64_t>(testcase.first.data());
+        EXPECT_EQ(res.first, testcase.second) /* << hex(testcase.first)*/;
+        EXPECT_EQ(res.second, &testcase.first[0] + testcase.first.size())
+        /* << hex(testcase.first)*/;
+    }
 }
 
 TEST(leb128, decode_u64_invalid)
@@ -51,10 +37,45 @@ TEST(leb128, decode_u64_invalid)
 
     fizzy::bytes encoded_1_too_many_leading_zeroes{
         0x81, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80};
-    EXPECT_THROW(
-        fizzy::leb128_decode_u64(encoded_1_too_many_leading_zeroes.data()), std::runtime_error);
+    EXPECT_THROW(fizzy::leb128u_decode<uint64_t>(encoded_1_too_many_leading_zeroes.data()),
+        std::runtime_error);
 
     fizzy::bytes encoded_max_leading_zeroes{
         0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x81, 0x00};
-    EXPECT_THROW(fizzy::leb128_decode_u64(encoded_max_leading_zeroes.data()), std::runtime_error);
+    EXPECT_THROW(
+        fizzy::leb128u_decode<uint64_t>(encoded_max_leading_zeroes.data()), std::runtime_error);
+}
+
+TEST(leb128, decode_u8)
+{
+    // clang-format off
+    std::vector<std::pair<fizzy::bytes, uint64_t>> testcases = {
+        {{0}, 0}, // 0
+        {{0x80, 0x00}, 0}, // 0 with leading zero
+        {{1}, 1}, // 1
+        {{0x81, 0x00}, 1}, // 1 with leading zero
+        {{0xe5, 0x01}, 229}, // 229
+        {{0xff, 0x01}, 255}, // max
+        // this is not really correct encoding, but is not rejected by our decoder
+        {{0xff, 0x7f}, 255},
+    };
+    // clang-format on
+
+    for (auto const& testcase : testcases)
+    {
+        auto res = fizzy::leb128u_decode<uint8_t>(testcase.first.data());
+        EXPECT_EQ(res.first, testcase.second) /* << hex(testcase.first)*/;
+        EXPECT_EQ(res.second, &testcase.first[0] + testcase.first.size())
+        /* << hex(testcase.first)*/;
+    }
+}
+
+TEST(leb128, decode_u8_invalid)
+{
+    fizzy::bytes encoded_1_too_many_leading_zeroes{0x81, 0x80, 0x80};
+    EXPECT_THROW(fizzy::leb128u_decode<uint8_t>(encoded_1_too_many_leading_zeroes.data()),
+        std::runtime_error);
+
+    fizzy::bytes encoded_too_big{0xe5, 0x8e, 0x26};
+    EXPECT_THROW(fizzy::leb128u_decode<uint8_t>(encoded_too_big.data()), std::runtime_error);
 }
