875bd5b3ab17ab11a7e90171c92f26fbac7c1096
Alex Beregszaszi <alex@rtfs.hu>
2020-01-21 19:31:41 +0000

    Rename limits to CamelCase
    page_size to PageSize and memory_pages_limit to MemoryPagesLimit

--- lib/fizzy/limits.hpp
+++ lib/fizzy/limits.hpp
@@ -1,6 +1,6 @@
 namespace fizzy
 {
-constexpr unsigned page_size = 65536;
+constexpr unsigned PageSize = 65536;
 // Set hard limit of 256MB of memory.
-constexpr unsigned memory_pages_limit = (256 * 1024 * 1024ULL) / page_size;
+constexpr unsigned MemoryPagesLimit = (256 * 1024 * 1024ULL) / PageSize;
 }  // namespace fizzy
