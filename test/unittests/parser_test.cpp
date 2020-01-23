746bf86fc96544cd20ab03624f0c5b0283207c6f
Paweł Bylica <chfast@gmail.com>
2020-01-23 16:36:39 +0100

    Use _bytes literal in parser tests

--- test/unittests/parser_test.cpp
+++ test/unittests/parser_test.cpp
@@ -5,9 +5,9 @@
 
 using namespace fizzy;
 
-static const auto functype_void_to_void = from_hex("600000");
-static const auto functype_i32i64_to_i32 = from_hex("60027f7e017f");
-static const auto functype_i32_to_void = from_hex("60017f00");
+static const auto functype_void_to_void = "600000"_bytes;
+static const auto functype_i32i64_to_i32 = "60027f7e017f"_bytes;
+static const auto functype_i32_to_void = "60017f00"_bytes;
 
 namespace
 {
@@ -31,7 +31,7 @@ TEST(parser, valtype)
 
 TEST(parser, valtype_vec)
 {
-    const auto input = from_hex("037f7e7fcc");
+    const auto input = "037f7e7fcc"_bytes;
     const auto [vec, pos] = parser<std::vector<ValType>>{}(input.data());
     EXPECT_EQ(pos, input.data() + 4);
     ASSERT_EQ(vec.size(), 3);
@@ -42,7 +42,7 @@ TEST(parser, valtype_vec)
 
 TEST(parser, limits_min)
 {
-    const auto input = from_hex("007f");
+    const auto input = "007f"_bytes;
     const auto [limits, pos] = parser<Limits>{}(input.data());
     EXPECT_EQ(limits.min, 0x7f);
     EXPECT_FALSE(limits.max.has_value());
@@ -50,7 +50,7 @@ TEST(parser, limits_min)
 
 TEST(parser, limits_minmax)
 {
-    const auto input = from_hex("01207f");
+    const auto input = "01207f"_bytes;
     const auto [limits, pos] = parser<Limits>{}(input.data());
     EXPECT_EQ(limits.min, 0x20);
     EXPECT_TRUE(limits.max.has_value());
@@ -59,25 +59,25 @@ TEST(parser, limits_minmax)
 
 TEST(parser, DISABLED_limits_min_invalid_too_short)
 {
-    const auto input = from_hex("00");
+    const auto input = "00"_bytes;
     EXPECT_THROW(parser<Limits>{}(input.data()), parser_error);
 }
 
 TEST(parser, DISABLED_limits_minmax_invalid_too_short)
 {
-    const auto input = from_hex("0120");
+    const auto input = "0120"_bytes;
     EXPECT_THROW(parser<Limits>{}(input.data()), parser_error);
 }
 
 TEST(parser, limits_invalid)
 {
-    const auto input = from_hex("02");
+    const auto input = "02"_bytes;
     EXPECT_THROW(parser<Limits>{}(input.data()), parser_error);
 }
 
 TEST(parser, locals)
 {
-    const auto input = from_hex("81017f");
+    const auto input = "81017f"_bytes;
     const auto [l, p] = parser<Locals>{}(input.data());
     EXPECT_EQ(l.count, 0x81);
     EXPECT_EQ(l.type, ValType::i32);
@@ -119,7 +119,7 @@ TEST(parser, custom_section_nonempty)
 
 TEST(parser, functype_wrong_prefix)
 {
-    const auto section_contents = uint8_t{0x01} + from_hex("610000");
+    const auto section_contents = uint8_t{0x01} + "610000"_bytes;
     const auto bin = bytes{wasm_prefix} + make_section(1, section_contents);
     EXPECT_THROW(parse(bin), parser_error);
 }
@@ -440,7 +440,7 @@ TEST(parser, start)
 TEST(parser, code_with_empty_expr_2_locals)
 {
     // Func with 2x i32 locals, only 0x0b "end" instruction.
-    const auto func_2_locals_bin = from_hex("01027f0b");
+    const auto func_2_locals_bin = "01027f0b"_bytes;
 
     const auto code_bin = uint8_t(func_2_locals_bin.size()) + func_2_locals_bin;
 
@@ -454,7 +454,7 @@ TEST(parser, code_with_empty_expr_2_locals)
 TEST(parser, code_with_empty_expr_5_locals)
 {
     // Func with 1x i64 + 4x i32 locals , only 0x0b "end" instruction.
-    const auto func_5_locals_bin = from_hex("02017f047e0b");
+    const auto func_5_locals_bin = "02017f047e0b"_bytes;
 
     const auto code_bin = uint8_t(func_5_locals_bin.size()) + func_5_locals_bin;
 
@@ -467,7 +467,7 @@ TEST(parser, code_with_empty_expr_5_locals)
 
 TEST(parser, code_section_with_2_trivial_codes)
 {
-    const auto func_nolocals_bin = from_hex("000b");
+    const auto func_nolocals_bin = "000b"_bytes;
     const auto code_bin = uint8_t(func_nolocals_bin.size()) + func_nolocals_bin;
     const auto section_contents = uint8_t{2} + code_bin + code_bin;
     const auto bin = bytes{wasm_prefix} + make_section(10, section_contents);
@@ -485,9 +485,9 @@ TEST(parser, code_section_with_2_trivial_codes)
 
 TEST(parser, code_section_with_basic_instructions)
 {
-    const auto func_bin = from_hex(
+    const auto func_bin =
         "00"  // vec(locals)
-        "2001210222036a01000b");
+        "2001210222036a01000b"_bytes;
     const auto code_bin = uint8_t(func_bin.size()) + func_bin;
     const auto section_contents = uint8_t{1} + code_bin;
     const auto bin = bytes{wasm_prefix} + make_section(10, section_contents);
@@ -505,7 +505,7 @@ TEST(parser, code_section_with_basic_instructions)
     EXPECT_EQ(module.codesec[0].instructions[5], Instr::unreachable);
     EXPECT_EQ(module.codesec[0].instructions[6], Instr::end);
     ASSERT_EQ(module.codesec[0].immediates.size(), 3 * 4);
-    EXPECT_EQ(module.codesec[0].immediates, from_hex("010000000200000003000000"));
+    EXPECT_EQ(module.codesec[0].immediates, "010000000200000003000000"_bytes);
 }
 
 TEST(parser, data_section)
@@ -518,13 +518,13 @@ TEST(parser, data_section)
     ASSERT_EQ(module.datasec.size(), 3);
     EXPECT_EQ(module.datasec[0].offset.kind, ConstantExpression::Kind::Constant);
     EXPECT_EQ(module.datasec[0].offset.value.constant, 1);
-    EXPECT_EQ(module.datasec[0].init, from_hex("aaff"));
+    EXPECT_EQ(module.datasec[0].init, "aaff"_bytes);
     EXPECT_EQ(module.datasec[1].offset.kind, ConstantExpression::Kind::Constant);
     EXPECT_EQ(module.datasec[1].offset.value.constant, 2);
-    EXPECT_EQ(module.datasec[1].init, from_hex("5555"));
+    EXPECT_EQ(module.datasec[1].init, "5555"_bytes);
     EXPECT_EQ(module.datasec[2].offset.kind, ConstantExpression::Kind::GlobalGet);
     EXPECT_EQ(module.datasec[2].offset.value.global_index, 0);
-    EXPECT_EQ(module.datasec[2].init, from_hex("2424"));
+    EXPECT_EQ(module.datasec[2].init, "2424"_bytes);
 }
 
 TEST(parser, data_section_memidx_nonzero)
@@ -553,8 +553,8 @@ TEST(parser, milestone1)
     )
     */
 
-    const auto bin = from_hex(
-        "0061736d0100000001070160027f7f017f030201000a13011101017f200020016a20026a220220006a0b");
+    const auto bin =
+        "0061736d0100000001070160027f7f017f030201000a13011101017f200020016a20026a220220006a0b"_bytes;
     const auto m = parse(bin);
 
     ASSERT_EQ(m.typesec.size(), 1);
@@ -567,54 +567,55 @@ TEST(parser, milestone1)
     EXPECT_EQ(c.instructions,
         (std::vector{Instr::local_get, Instr::local_get, Instr::i32_add, Instr::local_get,
             Instr::i32_add, Instr::local_tee, Instr::local_get, Instr::i32_add, Instr::end}));
-    EXPECT_EQ(c.immediates, from_hex("00000000"
-                                     "01000000"
-                                     "02000000"
-                                     "02000000"
-                                     "00000000"));
+    EXPECT_EQ(c.immediates,
+        "00000000"
+        "01000000"
+        "02000000"
+        "02000000"
+        "00000000"_bytes);
 }
 
 TEST(parser, instr_loop)
 {
-    const auto loop_without_imm = from_hex("030b");
+    const auto loop_without_imm = "030b"_bytes;
     EXPECT_THROW(parse_expr(loop_without_imm.data()), parser_error);
 
-    const auto loop_missing_end = from_hex("03400b");
+    const auto loop_missing_end = "03400b"_bytes;
     EXPECT_THROW(parse_expr(loop_missing_end.data()), parser_error);
 
-    const auto loop_void_empty = from_hex("03400b0b");
+    const auto loop_void_empty = "03400b0b"_bytes;
     const auto [code1, pos1] = parse_expr(loop_void_empty.data());
     EXPECT_EQ(code1.instructions, (std::vector{Instr::loop, Instr::end, Instr::end}));
     EXPECT_EQ(code1.immediates.size(), 0);
 
-    const auto loop_i32_empty = from_hex("037f0b0b");
+    const auto loop_i32_empty = "037f0b0b"_bytes;
     EXPECT_THROW(parse_expr(loop_i32_empty.data()), parser_error);  // loop arity != 0.
 }
 
 TEST(parser, instr_block)
 {
-    const auto wrong_type = from_hex("0200");
+    const auto wrong_type = "0200"_bytes;
     EXPECT_THROW(parse_expr(wrong_type.data()), parser_error);
 
-    const auto block_missing_end = from_hex("02400b");
+    const auto block_missing_end = "02400b"_bytes;
     EXPECT_THROW(parse_expr(block_missing_end.data()), parser_error);
 
-    const auto empty = from_hex("010102400b0b");
+    const auto empty = "010102400b0b"_bytes;
     const auto [code1, pos1] = parse_expr(empty.data());
     EXPECT_EQ(code1.instructions,
         (std::vector{Instr::nop, Instr::nop, Instr::block, Instr::end, Instr::end}));
-    EXPECT_EQ(hex(code1.immediates),
+    EXPECT_EQ(code1.immediates,
         "00"
         "04000000"
-        "09000000");
+        "09000000"_bytes);
 
-    const auto block_i64 = from_hex("027e0b0b");
+    const auto block_i64 = "027e0b0b"_bytes;
     const auto [code2, pos2] = parse_expr(block_i64.data());
     EXPECT_EQ(code2.instructions, (std::vector{Instr::block, Instr::end, Instr::end}));
-    EXPECT_EQ(hex(code2.immediates),
+    EXPECT_EQ(code2.immediates,
         "01"
         "02000000"
-        "09000000");
+        "09000000"_bytes);
 }
 
 TEST(parser, block_br)
@@ -630,18 +631,19 @@ TEST(parser, block_br)
     // local.get 1
     // end
 
-    const auto code_bin = from_hex("010240410a21010c00410b21010b20010b");
+    const auto code_bin = "010240410a21010c00410b21010b20010b"_bytes;
     const auto [code, pos] = parse_expr(code_bin.data());
     EXPECT_EQ(code.instructions,
         (std::vector{Instr::nop, Instr::block, Instr::i32_const, Instr::local_set, Instr::br,
             Instr::i32_const, Instr::local_set, Instr::end, Instr::local_get, Instr::end}));
-    EXPECT_EQ(hex(code.immediates), hex(from_hex("00"
-                                                 "08000000"
-                                                 "1d000000"
-                                                 "0a000000"
-                                                 "01000000"
-                                                 "00000000"
-                                                 "0b000000"
-                                                 "01000000"
-                                                 "01000000")));
+    EXPECT_EQ(code.immediates,
+        "00"
+        "08000000"
+        "1d000000"
+        "0a000000"
+        "01000000"
+        "00000000"
+        "0b000000"
+        "01000000"
+        "01000000"_bytes);
 }
