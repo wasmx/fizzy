cec47744d29df22333f545288bc4f79c55539647
Paweł Bylica <chfast@gmail.com>
2020-01-29 22:11:57 +0100

    cmake: Upgrade benchmark to 1.5.0

--- cmake/Hunter/init.cmake
+++ cmake/Hunter/init.cmake
@@ -19,4 +19,5 @@ set(HUNTER_CONFIGURATION_TYPES Release)
 HunterGate(
     URL https://github.com/cpp-pm/hunter/archive/v0.23.239.tar.gz
     SHA1 135567a8493ab3499187bce1f2a8df9b449febf3
+    LOCAL
 )
