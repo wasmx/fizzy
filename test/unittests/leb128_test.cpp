9b89ae7d70ae06f078f4bf05ed393f92d3e088c1
Andrei Maiboroda <andrei@ethereum.org>
2020-02-18 15:18:51 +0100

    tests: add tests for LEB128 unsigned 32-bit decoder

--- test/unittests/leb128_test.cpp
+++ test/unittests/leb128_test.cpp
@@ -33,8 +33,12 @@ TEST(leb128, decode_u64)
         {{0x81, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x00}, 1}, // 1 with max leading zeroes
         {{0xe5, 0x8e, 0x26}, 624485}, // 624485
         {{0xe5, 0x8e, 0xa6, 0x80, 0x80, 0x00}, 624485}, // 624485 with leading zeroes
+        {{0xff, 0xff, 0xff, 0xff, 0x07}, 0x7fffffff},
+        {{0x80, 0x80, 0x80, 0x80, 0x08}, 0x80000000},
         {{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00}, 562949953421311}, // bigger than int32
         {{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x80, 0x80, 0x00}, 562949953421311}, // bigger than int32 with zeroes
+        {{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7f}, 0x7fffffffffffffff},
+        {{0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x01}, 0x8000000000000000},
         {{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x01}, std::numeric_limits<uint64_t>::max()}, // max
         // this is not really correct encoding, but is not rejected by our decoder
         {{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7f}, std::numeric_limits<uint64_t>::max()},
@@ -63,6 +67,48 @@ TEST(leb128, decode_u64_invalid)
     bytes encoded_max_leading_zeroes{
         0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x81, 0x00};
     EXPECT_THROW_MESSAGE(leb128u_decode<uint64_t>(encoded_max_leading_zeroes), parser_error,
+        "Invalid LEB128 encoding: too many bytes.");
+}
+
+TEST(leb128, decode_u32)
+{
+    // clang-format off
+    std::vector<std::pair<bytes, uint32_t>> testcases = {
+        {{0}, 0}, // 0
+        {{0x80, 0x80, 0x00}, 0}, // 0 with leading zeroes
+        {{1}, 1}, // 1
+        {{0x81, 0x80, 0x80, 0x00}, 1}, // 1 with leading zeroes
+        {{0x81, 0x80, 0x80, 0x80, 0x00}, 1}, // 1 with max leading zeroes
+        {{0xe5, 0x8e, 0x26}, 624485}, // 624485
+        {{0xe5, 0x8e, 0xa6, 0x80, 0x00}, 624485}, // 624485 with leading zeroes
+        {{0xff, 0xff, 0xff, 0xff, 0x07}, 0x7fffffff},
+        {{0x80, 0x80, 0x80, 0x80, 0x08}, 0x80000000},
+        {{0xff, 0xff, 0xff, 0xff, 0x0f}, std::numeric_limits<uint32_t>::max()}, // max
+        // this is not really correct encoding, but is not rejected by our decoder
+        {{0xff, 0xff, 0xff, 0xff, 0x7f}, std::numeric_limits<uint32_t>::max()},
+    };
+    // clang-format on
+
+    for (auto const& testcase : testcases)
+    {
+        auto res = leb128u_decode<uint32_t>(testcase.first);
+        EXPECT_EQ(res.first, testcase.second) << hex(testcase.first);
+        EXPECT_EQ(res.second, &testcase.first[0] + testcase.first.size()) << hex(testcase.first);
+    }
+}
+
+TEST(leb128, decode_u32_invalid)
+{
+    bytes encoded_624485_invalid{0xe5, 0x8e, 0xa6};
+    EXPECT_THROW_MESSAGE(
+        leb128u_decode<uint32_t>(encoded_624485_invalid), parser_error, "Unexpected EOF");
+
+    bytes encoded_1_too_many_leading_zeroes{0x81, 0x80, 0x80, 0x80, 0x80, 0x00};
+    EXPECT_THROW_MESSAGE(leb128u_decode<uint32_t>(encoded_1_too_many_leading_zeroes), parser_error,
+        "Invalid LEB128 encoding: too many bytes.");
+
+    bytes encoded_max_leading_zeroes{0xff, 0xff, 0xff, 0xff, 0xff, 0x00};
+    EXPECT_THROW_MESSAGE(leb128u_decode<uint32_t>(encoded_max_leading_zeroes), parser_error,
         "Invalid LEB128 encoding: too many bytes.");
 }
 
