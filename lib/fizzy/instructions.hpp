5cf557f798b99fe69df8bc0192357f73316ed425
Paweł Bylica <chfast@gmail.com>
2020-03-25 16:32:49 +0100

    WIP instruction metrics table

--- lib/fizzy/instructions.hpp
+++ lib/fizzy/instructions.hpp
@@ -4,6 +4,18 @@
 
 namespace fizzy
 {
+struct InstructionMetrics
+{
+    /// The minimum number of the stack items required for the instruction.
+    int8_t stack_height_required;
+
+    /// The EVM stack height change caused by the instruction execution,
+    /// i.e. stack height _after_ execution - stack height _before_ execution.
+    int8_t stack_height_change;
+};
+
+InstructionMetrics* get_instruction_metrics_table() noexcept;
+
 ssize_t instr_stack_change(Instr instr);
 
 }  // namespace fizzy
