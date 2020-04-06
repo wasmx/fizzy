0288040acc74c88a95522f90d1c0aa7825a7a836
Alex Beregszaszi <alex@rtfs.hu>
2020-04-06 14:14:38 +0100

    tests: introduce support for gmock's Matcher API on execution_result

--- test/utils/asserts.hpp
+++ test/utils/asserts.hpp
@@ -1,6 +1,21 @@
 #pragma once
 
-#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+MATCHER(Traps, "")
+{
+    return arg.trapped;
+}
+
+MATCHER(Result, "empty result")
+{
+    return !arg.trapped && arg.stack.size() == 0;
+}
+
+MATCHER_P(Result, value, "")
+{
+    return !arg.trapped && arg.stack.size() == 1 && arg.stack[0] == uint64_t(value);
+}
 
 #define EXPECT_RESULT(result, expected)    \
     do                                     \
