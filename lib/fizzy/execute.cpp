ec402e40f7ddf1575210124e72526a77e45fe2f8
Paweł Bylica <chfast@gmail.com>
2020-04-09 10:41:20 +0200

    Change stack.peek(x) to stack[x] and stack.top()

--- lib/fizzy/execute.cpp
+++ lib/fizzy/execute.cpp
@@ -268,7 +268,7 @@ void branch(uint32_t label_idx, LabelStack& labels, Stack<uint64_t>& stack, cons
     if (label.arity != 0)
     {
         assert(label.arity == 1);
-        const auto result = stack.peek();
+        const auto result = stack.top();
         stack.resize(label.stack_height);
         stack.push(result);
     }
@@ -860,7 +860,7 @@ execution_result execute(
         {
             const auto idx = read<uint32_t>(immediates);
             assert(idx <= locals.size());
-            locals[idx] = stack.peek();
+            locals[idx] = stack.top();
             break;
         }
         case Instr::global_get:
@@ -1235,8 +1235,8 @@ execution_result execute(
         }
         case Instr::i32_div_s:
         {
-            auto const rhs = static_cast<int32_t>(stack.peek(0));
-            auto const lhs = static_cast<int32_t>(stack.peek(1));
+            auto const rhs = static_cast<int32_t>(stack[0]);
+            auto const lhs = static_cast<int32_t>(stack[1]);
             if (rhs == 0 || (lhs == std::numeric_limits<int32_t>::min() && rhs == -1))
             {
                 trap = true;
@@ -1247,7 +1247,7 @@ execution_result execute(
         }
         case Instr::i32_div_u:
         {
-            auto const rhs = static_cast<uint32_t>(stack.peek());
+            auto const rhs = static_cast<uint32_t>(stack.top());
             if (rhs == 0)
             {
                 trap = true;
@@ -1258,13 +1258,13 @@ execution_result execute(
         }
         case Instr::i32_rem_s:
         {
-            auto const rhs = static_cast<int32_t>(stack.peek());
+            auto const rhs = static_cast<int32_t>(stack.top());
             if (rhs == 0)
             {
                 trap = true;
                 goto end;
             }
-            auto const lhs = static_cast<int32_t>(stack.peek(1));
+            auto const lhs = static_cast<int32_t>(stack[1]);
             if (lhs == std::numeric_limits<int32_t>::min() && rhs == -1)
             {
                 stack.drop(2);
@@ -1276,7 +1276,7 @@ execution_result execute(
         }
         case Instr::i32_rem_u:
         {
-            auto const rhs = static_cast<uint32_t>(stack.peek());
+            auto const rhs = static_cast<uint32_t>(stack.top());
             if (rhs == 0)
             {
                 trap = true;
@@ -1357,8 +1357,8 @@ execution_result execute(
         }
         case Instr::i64_div_s:
         {
-            auto const rhs = static_cast<int64_t>(stack.peek(0));
-            auto const lhs = static_cast<int64_t>(stack.peek(1));
+            auto const rhs = static_cast<int64_t>(stack[0]);
+            auto const lhs = static_cast<int64_t>(stack[1]);
             if (rhs == 0 || (lhs == std::numeric_limits<int64_t>::min() && rhs == -1))
             {
                 trap = true;
@@ -1369,7 +1369,7 @@ execution_result execute(
         }
         case Instr::i64_div_u:
         {
-            auto const rhs = static_cast<uint64_t>(stack.peek());
+            auto const rhs = static_cast<uint64_t>(stack.top());
             if (rhs == 0)
             {
                 trap = true;
@@ -1380,13 +1380,13 @@ execution_result execute(
         }
         case Instr::i64_rem_s:
         {
-            auto const rhs = static_cast<int64_t>(stack.peek());
+            auto const rhs = static_cast<int64_t>(stack.top());
             if (rhs == 0)
             {
                 trap = true;
                 goto end;
             }
-            auto const lhs = static_cast<int64_t>(stack.peek(1));
+            auto const lhs = static_cast<int64_t>(stack[1]);
             if (lhs == std::numeric_limits<int64_t>::min() && rhs == -1)
             {
                 stack.drop(2);
@@ -1398,7 +1398,7 @@ execution_result execute(
         }
         case Instr::i64_rem_u:
         {
-            auto const rhs = static_cast<uint64_t>(stack.peek());
+            auto const rhs = static_cast<uint64_t>(stack.top());
             if (rhs == 0)
             {
                 trap = true;
