19afdca593fa1454d51f1fe1276e53f3e879c70f
Paweł Bylica <chfast@gmail.com>
2020-04-15 12:44:50 +0200

    Add OperandStack class

--- lib/fizzy/stack.hpp
+++ lib/fizzy/stack.hpp
@@ -6,6 +6,7 @@
 
 #include <cassert>
 #include <cstdint>
+#include <memory>
 #include <vector>
 
 namespace fizzy
@@ -47,5 +48,80 @@ public:
         assert(new_size <= size());
         resize(new_size);
     }
+};
+
+class OperandStack
+{
+    /// The pointer to the top item, or below the stack bottom if stack is empty.
+    ///
+    /// This pointer always alias m_storage, but it is kept as the first field
+    /// because it is accessed the most. Therefore, it must be initialized
+    /// in the constructor after the m_storage.
+    uint64_t* m_top;
+
+    /// The storage for items.
+    std::unique_ptr<uint64_t[]> m_storage;
+
+public:
+    /// Default constructor. Sets the top item pointer to below the stack bottom.
+    explicit OperandStack(size_t max_stack_height)
+      : m_storage{std::make_unique<uint64_t[]>(max_stack_height)}
+    {
+        m_top = m_storage.get() - 1;
+    }
+
+    OperandStack(const OperandStack&) = delete;
+    OperandStack& operator=(const OperandStack&) = delete;
+
+    /// The current number of items on the stack (aka stack height).
+    [[nodiscard]] size_t size() const noexcept
+    {
+        return static_cast<size_t>(m_top + 1 - m_storage.get());
+    }
+
+    /// Returns the reference to the top item.
+    /// Requires non-empty stack.
+    [[nodiscard]] auto& top() noexcept
+    {
+        assert(size() != 0);
+        return *m_top;
+    }
+
+    /// Returns the reference to the stack item on given position from the stack top.
+    /// Requires index < size().
+    [[nodiscard]] auto& operator[](size_t index) noexcept
+    {
+        assert(index < size());
+        return *(m_top - index);
+    }
+
+    /// Pushes an item on the stack.
+    /// The stack max height limit is not checked.
+    void push(uint64_t item) noexcept { *++m_top = item; }
+
+    /// Returns an item popped from the top of the stack.
+    /// Requires non-empty stack.
+    auto pop() noexcept
+    {
+        assert(size() != 0);
+        return *m_top--;
+    }
+
+    /// Shrinks the stack to the given new size by dropping items from the top.
+    ///
+    /// Requires new_size <= size().
+    /// shrink(0) clears entire stack and moves the top pointer below the stack base.
+    void shrink(size_t new_size) noexcept
+    {
+        assert(new_size <= size());
+        // For new_size == 0, the m_top will point below the storage.
+        m_top = m_storage.get() + new_size - 1;
+    }
+
+    /// Returns iterator to the bottom of the stack.
+    [[nodiscard]] const uint64_t* rbegin() const noexcept { return m_storage.get(); }
+
+    /// Returns end iterator counting from the bottom of the stack.
+    [[nodiscard]] const uint64_t* rend() const noexcept { return m_top + 1; }
 };
 }  // namespace fizzy
--- test/unittests/stack_test.cpp
+++ test/unittests/stack_test.cpp
@@ -3,9 +3,10 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "stack.hpp"
-#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 using namespace fizzy;
+using namespace testing;
 
 TEST(stack, push_and_pop)
 {
@@ -144,4 +145,120 @@ TEST(stack, clear_on_empty)
 {
     Stack<char> stack;
     stack.clear();
+}
+
+TEST(operand_stack, construct)
+{
+    OperandStack stack(0);
+    EXPECT_EQ(stack.size(), 0);
+    stack.shrink(0);
+    EXPECT_EQ(stack.size(), 0);
+}
+
+TEST(operand_stack, top)
+{
+    OperandStack stack(1);
+    EXPECT_EQ(stack.size(), 0);
+
+    stack.push(1);
+    EXPECT_EQ(stack.size(), 1);
+    EXPECT_EQ(stack.top(), 1);
+    EXPECT_EQ(stack[0], 1);
+
+    stack.top() = 101;
+    EXPECT_EQ(stack.size(), 1);
+    EXPECT_EQ(stack.top(), 101);
+    EXPECT_EQ(stack[0], 101);
+
+    stack.shrink(0);
+    EXPECT_EQ(stack.size(), 0);
+
+    stack.push(2);
+    EXPECT_EQ(stack.size(), 1);
+    EXPECT_EQ(stack.top(), 2);
+    EXPECT_EQ(stack[0], 2);
+}
+
+TEST(operand_stack, small)
+{
+    OperandStack stack(3);
+    EXPECT_EQ(stack.size(), 0);
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    EXPECT_EQ(stack.size(), 3);
+    EXPECT_EQ(stack.top(), 3);
+    EXPECT_EQ(stack[0], 3);
+    EXPECT_EQ(stack[1], 2);
+    EXPECT_EQ(stack[2], 1);
+
+    stack[0] = 13;
+    stack[1] = 12;
+    stack[2] = 11;
+    EXPECT_EQ(stack.size(), 3);
+    EXPECT_EQ(stack.top(), 13);
+    EXPECT_EQ(stack[0], 13);
+    EXPECT_EQ(stack[1], 12);
+    EXPECT_EQ(stack[2], 11);
+
+    EXPECT_EQ(stack.pop(), 13);
+    EXPECT_EQ(stack.size(), 2);
+    EXPECT_EQ(stack.top(), 12);
+}
+
+TEST(operand_stack, large)
+{
+    constexpr auto max_height = 33;
+    OperandStack stack(max_height);
+
+    for (unsigned i = 0; i < max_height; ++i)
+        stack.push(i);
+
+    EXPECT_EQ(stack.size(), max_height);
+    for (int expected = max_height - 1; expected >= 0; --expected)
+        EXPECT_EQ(stack.pop(), expected);
+    EXPECT_EQ(stack.size(), 0);
+}
+
+TEST(operand_stack, shrink)
+{
+    constexpr auto max_height = 60;
+    OperandStack stack(max_height);
+
+    for (unsigned i = 0; i < max_height; ++i)
+        stack.push(i);
+
+    EXPECT_EQ(stack.size(), max_height);
+    constexpr auto new_height = max_height / 3;
+    stack.shrink(new_height);
+    EXPECT_EQ(stack.size(), new_height);
+    EXPECT_EQ(stack.top(), new_height - 1);
+    EXPECT_EQ(stack[0], new_height - 1);
+    EXPECT_EQ(stack[new_height - 1], 0);
+}
+
+TEST(operand_stack, rbegin_rend)
+{
+    OperandStack stack(3);
+    EXPECT_EQ(stack.rbegin(), stack.rend());
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    EXPECT_LT(stack.rbegin(), stack.rend());
+    EXPECT_EQ(*stack.rbegin(), 1);
+    EXPECT_EQ(*(stack.rend() - 1), 3);
+}
+
+TEST(operand_stack, to_vector)
+{
+    OperandStack stack(3);
+    EXPECT_THAT(std::vector(stack.rbegin(), stack.rend()), IsEmpty());
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    EXPECT_THAT(std::vector(stack.rbegin(), stack.rend()), ElementsAre(1, 2, 3));
 }
