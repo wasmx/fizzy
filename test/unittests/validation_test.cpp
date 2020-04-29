116b2824cd33a19560d175df28b3d3ebf5a3b317
Andrei Maiboroda <andrei@ethereum.org>
2020-04-29 16:19:47 +0200

    test: call_indirect type index validation

--- test/unittests/validation_test.cpp
+++ test/unittests/validation_test.cpp
@@ -206,3 +206,17 @@ TEST(validation, call_unknown_function)
         from_hex("0061736d010000000108026000006000017f020701016d01660000030201010a0601040010020b");
     EXPECT_THROW_MESSAGE(parse(wasm), validation_error, "invalid funcidx encountered with call");
 }
+
+TEST(validation, call_indirect_unknown_type)
+{
+    /* wat2wasm --no-check
+    (table anyfunc (elem 0))
+    (func (param i32)
+      (call_indirect (type 1) (get_local 0))
+    )
+    */
+    const auto wasm = from_hex(
+        "0061736d0100000001050160017f0003020100040501700101010907010041000b01000a090107002000110100"
+        "0b");
+    EXPECT_THROW_MESSAGE(parse(wasm), validation_error, "invalid type index with call_indirect");
+}
