a6ed6e8f79e32388c0ded112f10c83adeccbdfd0
Alex Beregszaszi <alex@rtfs.hu>
2020-05-19 22:20:33 +0100

    test: add adler32 helper in utils

---
+++ test/utils/adler32.hpp
@@ -0,0 +1,28 @@
+// Fizzy: A fast WebAssembly interpreter
+// Copyright 2020 The Fizzy Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include "bytes.hpp"
+
+namespace fizzy
+{
+// This calculates the Adler-32 checksum of the data.
+// It is used in benchmarking.
+inline uint32_t adler32(bytes_view data) noexcept
+{
+    constexpr uint32_t mod = 65521;
+    uint32_t a = 1;
+    uint32_t b = 0;
+
+    for (const auto v : data)
+    {
+        a = (a + v) % mod;
+        b = (b + a) % mod;
+    }
+
+    return (b << 16) | a;
+}
+
+}  // namespace fizzy
