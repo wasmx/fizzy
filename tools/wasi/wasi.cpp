2090c08a5f00c36b4e8d5a96f2a0d3858a7f3cff
Alex Beregszaszi <alex@rtfs.hu>
2020-05-22 12:23:03 +0100

    wasi: implementation of fd_write, fd_read, fd_prestat_get, and environ_sizes_get
    And also add two dummy implementations of fd_prestat_dir_name and environ_get.

--- tools/wasi/wasi.cpp
+++ tools/wasi/wasi.cpp
@@ -18,6 +18,11 @@ namespace
 // and we are a single-run tool. This may change in the future and should reevaluate.
 uvwasi_t state;
 
+fizzy::ExecutionResult wasi_return_enosys(fizzy::Instance&, fizzy::span<const fizzy::Value>, int)
+{
+    return fizzy::Value{uint32_t{UVWASI_ENOSYS}};
+}
+
 fizzy::ExecutionResult wasi_proc_exit(fizzy::Instance&, fizzy::span<const fizzy::Value> args, int)
 {
     uvwasi_proc_exit(&state, static_cast<uvwasi_exitcode_t>(args[0].as<uint32_t>()));
@@ -25,11 +30,95 @@ fizzy::ExecutionResult wasi_proc_exit(fizzy::Instance&, fizzy::span<const fizzy:
     return fizzy::Trap;
 }
 
+fizzy::ExecutionResult wasi_fd_write(
+    fizzy::Instance& instance, fizzy::span<const fizzy::Value> args, int)
+{
+    const auto fd = args[0].as<uint32_t>();
+    const auto iov_ptr = args[1].as<uint32_t>();
+    const auto iov_cnt = args[2].as<uint32_t>();
+    const auto nwritten_ptr = args[3].as<uint32_t>();
+
+    std::vector<uvwasi_ciovec_t> iovs(iov_cnt);
+    // TODO: not sure what to pass as end, passing memory size...
+    uvwasi_errno_t ret = uvwasi_serdes_readv_ciovec_t(
+        instance.memory->data(), instance.memory->size(), iov_ptr, iovs.data(), iov_cnt);
+    if (ret != UVWASI_ESUCCESS)
+        return fizzy::Value{uint32_t{ret}};
+
+    uvwasi_size_t nwritten;
+    ret = uvwasi_fd_write(&state, static_cast<uvwasi_fd_t>(fd), iovs.data(), iov_cnt, &nwritten);
+    uvwasi_serdes_write_uint32_t(instance.memory->data(), nwritten_ptr, nwritten);
+
+    return fizzy::Value{uint32_t{ret}};
+}
+
+fizzy::ExecutionResult wasi_fd_read(
+    fizzy::Instance& instance, fizzy::span<const fizzy::Value> args, int)
+{
+    const auto fd = args[0].as<uint32_t>();
+    const auto iov_ptr = args[1].as<uint32_t>();
+    const auto iov_cnt = args[2].as<uint32_t>();
+    const auto nread_ptr = args[3].as<uint32_t>();
+
+    std::vector<uvwasi_iovec_t> iovs(iov_cnt);
+    // TODO: not sure what to pass as end, passing memory size...
+    uvwasi_errno_t ret = uvwasi_serdes_readv_iovec_t(
+        instance.memory->data(), instance.memory->size(), iov_ptr, iovs.data(), iov_cnt);
+    if (ret != UVWASI_ESUCCESS)
+        return fizzy::Value{uint32_t{ret}};
+
+    uvwasi_size_t nread;
+    ret = uvwasi_fd_read(&state, static_cast<uvwasi_fd_t>(fd), iovs.data(), iov_cnt, &nread);
+    uvwasi_serdes_write_uint32_t(instance.memory->data(), nread_ptr, nread);
+
+    return fizzy::Value{uint32_t{ret}};
+}
+
+fizzy::ExecutionResult wasi_fd_prestat_get(
+    fizzy::Instance& instance, fizzy::span<const fizzy::Value> args, int)
+{
+    const auto fd = args[0].as<uint32_t>();
+    const auto prestat_ptr = args[1].as<uint32_t>();
+
+    uvwasi_prestat_t buf;
+    uvwasi_errno_t ret = uvwasi_fd_prestat_get(&state, fd, &buf);
+
+    uvwasi_serdes_write_prestat_t(instance.memory->data(), prestat_ptr, &buf);
+
+    return fizzy::Value{uint32_t{ret}};
+}
+
+fizzy::ExecutionResult wasi_environ_sizes_get(
+    fizzy::Instance& instance, fizzy::span<const fizzy::Value> args, int)
+{
+    const auto environc = args[0].as<uint32_t>();
+    const auto environ_buf_size = args[1].as<uint32_t>();
+    // TODO: implement properly (only returns 0 now)
+    uvwasi_serdes_write_uint32_t(instance.memory->data(), environc, 0);
+    uvwasi_serdes_write_uint32_t(instance.memory->data(), environ_buf_size, 0);
+    return fizzy::Value{uint32_t{UVWASI_ESUCCESS}};
+}
+
 bool run(int argc, const char** argv)
 {
     const std::vector<fizzy::ImportedFunction> wasi_functions = {
         {"wasi_snapshot_preview1", "proc_exit", {fizzy::ValType::i32}, std::nullopt,
             wasi_proc_exit},
+        {"wasi_snapshot_preview1", "fd_read",
+            {fizzy::ValType::i32, fizzy::ValType::i32, fizzy::ValType::i32, fizzy::ValType::i32},
+            fizzy::ValType::i32, wasi_fd_read},
+        {"wasi_snapshot_preview1", "fd_write",
+            {fizzy::ValType::i32, fizzy::ValType::i32, fizzy::ValType::i32, fizzy::ValType::i32},
+            fizzy::ValType::i32, wasi_fd_write},
+        {"wasi_snapshot_preview1", "fd_prestat_get", {fizzy::ValType::i32, fizzy::ValType::i32},
+            fizzy::ValType::i32, wasi_fd_prestat_get},
+        {"wasi_snapshot_preview1", "fd_prestat_dir_name",
+            {fizzy::ValType::i32, fizzy::ValType::i32, fizzy::ValType::i32}, fizzy::ValType::i32,
+            wasi_return_enosys},
+        {"wasi_snapshot_preview1", "environ_sizes_get", {fizzy::ValType::i32, fizzy::ValType::i32},
+            fizzy::ValType::i32, wasi_environ_sizes_get},
+        {"wasi_snapshot_preview1", "environ_get", {fizzy::ValType::i32, fizzy::ValType::i32},
+            fizzy::ValType::i32, wasi_return_enosys},
     };
 
     // Initialisation settings.
