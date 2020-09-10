11ff29dea936882b5f99b29eac89060ee99960c9
Paweł Bylica <chfast@gmail.com>
2020-09-10 20:38:47 +0200

    Add public C header fizzy.h

---
+++ include/fizzy/fizzy.h
@@ -0,0 +1,12 @@
+// Fizzy: A fast WebAssembly interpreter
+// Copyright 2020 The Fizzy Authors.
+// SPDX-License-Identifier: Apache-2.0
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
