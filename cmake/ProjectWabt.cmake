fd58c4efb926bbfbf62ccdb27feca2b454b7052f
Paweł Bylica <chfast@gmail.com>
2020-09-15 15:32:36 +0200

    cmake: Pass sanitizer flags to WABT

--- cmake/ProjectWabt.cmake
+++ cmake/ProjectWabt.cmake
@@ -11,6 +11,12 @@ set(binary_dir ${prefix}/src/wabt-build)
 set(include_dir ${source_dir})
 set(wabt_library ${binary_dir}/${CMAKE_STATIC_LIBRARY_PREFIX}wabt${CMAKE_STATIC_LIBRARY_SUFFIX})
 
+set(flags -fvisibility=hidden)
+if(SANITIZE MATCHES address)
+    # Instrument WABT with ASan - required for container-overflow checks.
+    set(flags "-fsanitize=address ${flags}")
+endif()
+
 ExternalProject_Add(wabt
     EXCLUDE_FROM_ALL 1
     PREFIX ${prefix}
@@ -29,8 +35,8 @@ ExternalProject_Add(wabt
     -DBUILD_TESTS=OFF
     -DBUILD_TOOLS=OFF
     -DCMAKE_POSITION_INDEPENDENT_CODE=FALSE
-    -DCMAKE_CXX_FLAGS=-fvisibility=hidden
-    -DCMAKE_C_FLAGS=-fvisibility=hidden
+    -DCMAKE_CXX_FLAGS=${flags}
+    -DCMAKE_C_FLAGS=${flags}
     INSTALL_COMMAND ""
     BUILD_BYPRODUCTS ${wabt_library}
 )
