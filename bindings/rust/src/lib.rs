0ae940ec6ada660b388ce7fed8ca05fc899a775b
Alex Beregszaszi <alex@rtfs.hu>
2020-09-30 20:27:49 +0100

    rust: add execute and Value/ExecutionResult

--- bindings/rust/src/lib.rs
+++ bindings/rust/src/lib.rs
@@ -3,8 +3,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 //! This is a Rust interface to [Fizzy](https://github.com/wasmx/fizzy), a WebAssembly virtual machine.
-//!
-//! Currently it is only possible to validate inputs, but soon execution will be supported too.
 
 mod sys;
 
@@ -72,6 +70,58 @@ impl Module {
     }
 }
 
+// TODO: add a proper wrapper (enum type for Value, and Result<Value, Trap> for ExecutionResult)
+
+/// A WebAssembly value of i32/i64/f32/f64.
+pub type Value = sys::FizzyValue;
+
+/// The result of an execution.
+pub struct ExecutionResult(sys::FizzyExecutionResult);
+
+impl ExecutionResult {
+    /// True if execution has resulted in a trap.
+    pub fn trapped(&self) -> bool {
+        self.0.trapped
+    }
+
+    /// The optional return value. Only a single return value is allowed in WebAssembly 1.0.
+    pub fn value(&self) -> Option<Value> {
+        if self.0.has_value {
+            assert!(!self.0.trapped);
+            Some(self.0.value)
+        } else {
+            None
+        }
+    }
+}
+
+impl From<ExecutionResult> for sys::FizzyExecutionResult {
+    fn from(v: ExecutionResult) -> Self {
+        v.0
+    }
+}
+
+impl Instance {
+    /// Unsafe execution of a given function index `func_idx` with the given values `args`.
+    ///
+    /// An invalid index, invalid inputs, or invalid depth can cause undefined behaviour.
+    ///
+    /// The `depth` argument sets the initial call depth. Should be set to `0`.
+    ///
+    /// # Safety
+    /// This function expects a valid `func_idx`, appropriate number of `args`, and valid `depth`.
+    pub unsafe fn unsafe_execute(
+        &mut self,
+        func_idx: u32,
+        args: &[Value],
+        depth: i32,
+    ) -> ExecutionResult {
+        ExecutionResult {
+            0: sys::fizzy_execute(self.0.as_ptr(), func_idx, args.as_ptr(), depth),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -106,5 +156,46 @@ mod tests {
         assert!(module.is_ok());
         let instance = module.unwrap().instantiate();
         assert!(instance.is_ok());
+    }
+
+    #[test]
+    fn unsafe_execute_wasm() {
+        /* wat2wasm
+          (func)
+          (func (result i32) i32.const 42)
+          (func (param i32 i32) (result i32)
+            (i32.div_u (local.get 0) (local.get 1))
+          )
+          (func unreachable)
+        */
+        let input = hex::decode("0061736d01000000010e036000006000017f60027f7f017f030504000102000a150402000b0400412a0b0700200020016e0b0300000b").unwrap();
+        let module = parse(&input);
+        assert!(module.is_ok());
+        let instance = module.unwrap().instantiate();
+        assert!(instance.is_ok());
+        let mut instance = instance.unwrap();
+
+        let result = unsafe { instance.unsafe_execute(0, &[], 0) };
+        assert!(!result.trapped());
+        assert!(!result.value().is_some());
+
+        let result = unsafe { instance.unsafe_execute(1, &[], 0) };
+        assert!(!result.trapped());
+        assert!(result.value().is_some());
+        unsafe {
+            assert_eq!(result.value().unwrap().i64, 42);
+        } // FIXME: this is actually i32
+
+        let result =
+            unsafe { instance.unsafe_execute(2, &[Value { i64: 42 }, Value { i64: 2 }], 0) };
+        assert!(!result.trapped());
+        assert!(result.value().is_some());
+        unsafe {
+            assert_eq!(result.value().unwrap().i64, 21);
+        } // FIXME: this is actually i32
+
+        let result = unsafe { instance.unsafe_execute(3, &[], 0) };
+        assert!(result.trapped());
+        assert!(!result.value().is_some());
     }
 }
