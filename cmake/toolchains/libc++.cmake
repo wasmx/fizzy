925a61a55ee3c5cd18dcc2066ee1c974cb54c52c
Paweł Bylica <chfast@gmail.com>
2020-10-13 14:48:51 +0200

    ci: Build "tidy" / C++20 job with libc++

---
+++ cmake/toolchains/libc++.cmake
@@ -0,0 +1,7 @@
+# Fizzy: A fast WebAssembly interpreter
+# Copyright 2020 The Fizzy Authors.
+# SPDX-License-Identifier: Apache-2.0
+
+set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_CXX_FLAGS_INIT -stdlib=libc++)
