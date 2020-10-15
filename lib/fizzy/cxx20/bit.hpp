ff82160f5034fdde7b402d3a0422ff937f2214b9
Paweł Bylica <chfast@gmail.com>
2020-10-15 22:53:12 +0200

    Provide constexpr bit_cast if possible

--- lib/fizzy/cxx20/bit.hpp
+++ lib/fizzy/cxx20/bit.hpp
@@ -11,22 +11,32 @@
 #include <version>
 #endif
 
+#ifndef __has_builtin
+#define __has_builtin(name) 0
+#endif
+
 namespace fizzy
 {
 /// The non-constexpr implementation of C++20's std::bit_cast.
 /// See https://en.cppreference.com/w/cpp/numeric/bit_cast.
 template <class To, class From>
 [[nodiscard]] inline
+#if __has_builtin(__builtin_bit_cast)
+    constexpr
+#endif
     typename std::enable_if_t<sizeof(To) == sizeof(From) && std::is_trivially_copyable_v<From> &&
                                   std::is_trivially_copyable_v<To>,
         To>
     bit_cast(const From& src) noexcept
 {
+#if __has_builtin(__builtin_bit_cast)
+    return __builtin_bit_cast(To, src);
+#else
     static_assert(std::is_trivially_constructible_v<To>);  // Additional, non-standard requirement.
-
     To dst;
     __builtin_memcpy(&dst, &src, sizeof(To));
     return dst;
+#endif
 }
 }  // namespace fizzy
 
