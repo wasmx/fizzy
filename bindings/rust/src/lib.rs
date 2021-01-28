1c100426e8e4d16dcf52803abec3c2c68e9869b9
Alex Beregszaszi <alex@rtfs.hu>
2021-01-28 18:32:14 +0000

    rust: add unit tests for Value

--- bindings/rust/src/lib.rs
+++ bindings/rust/src/lib.rs
@@ -200,6 +200,41 @@ mod tests {
     use super::*;
 
     #[test]
+    fn value_conversions() {
+        // NOTE: since the underlying type is a union, a conversion or access to other members is undefined
+
+        let v: Value = i32::MIN.into();
+        assert_eq!(v.as_i32(), i32::MIN);
+        let v: Value = i32::MAX.into();
+        assert_eq!(v.as_i32(), i32::MAX);
+
+        let v: Value = u32::MIN.into();
+        assert_eq!(v.as_u32(), u32::MIN);
+        let v: Value = u32::MAX.into();
+        assert_eq!(v.as_u32(), u32::MAX);
+
+        let v: Value = i64::MIN.into();
+        assert_eq!(v.as_i64(), i64::MIN);
+        let v: Value = i64::MAX.into();
+        assert_eq!(v.as_i64(), i64::MAX);
+
+        let v: Value = u64::MIN.into();
+        assert_eq!(v.as_u64(), u64::MIN);
+        let v: Value = u64::MAX.into();
+        assert_eq!(v.as_u64(), u64::MAX);
+
+        let v: Value = f32::MIN.into();
+        assert_eq!(v.as_f32(), f32::MIN);
+        let v: Value = f32::MAX.into();
+        assert_eq!(v.as_f32(), f32::MAX);
+
+        let v: Value = f64::MIN.into();
+        assert_eq!(v.as_f64(), f64::MIN);
+        let v: Value = f64::MAX.into();
+        assert_eq!(v.as_f64(), f64::MAX);
+    }
+
+    #[test]
     fn validate_wasm() {
         // Empty
         assert_eq!(validate(&[]), false);
