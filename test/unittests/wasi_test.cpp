05842f8ee8781563187677ef29f0bc9f05bf9b53
Paweł Bylica <chfast@gmail.com>
2021-02-01 21:20:32 +0100

    test: Add WASI unit tests

---
+++ test/unittests/wasi_test.cpp
@@ -0,0 +1,20 @@
+// Fizzy: A fast WebAssembly interpreter
+// Copyright 2021 The Fizzy Authors.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "wasi.hpp"
+#include <gtest/gtest.h>
+#include <test/utils/hex.hpp>
+#include <sstream>
+
+using namespace fizzy;
+using namespace fizzy::test;
+
+TEST(wasi, no_file)
+{
+    const char* args[]{"ABC"};
+
+    std::ostringstream err;
+    EXPECT_FALSE(wasi::run(std::size(args), args, err));
+    EXPECT_EQ(err.str(), "File does not exists or is irregular: ABC\n");
+}
