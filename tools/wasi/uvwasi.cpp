18440ee633d3b2ed933521fde8921ceb8a41833f
Andrei Maiboroda <andrei@ethereum.org>
2021-05-05 15:39:00 +0200

    wasi: Call uvwasi_destroy

--- tools/wasi/uvwasi.cpp
+++ tools/wasi/uvwasi.cpp
@@ -13,10 +13,20 @@ class UVWASIImpl final : public UVWASI
 {
     /// UVWASI state.
     uvwasi_t m_state{};
+    bool m_inited = false;
 
 public:
+    ~UVWASIImpl() final
+    {
+        if (m_inited)
+            uvwasi_destroy(&m_state);
+    }
+
     uvwasi_errno_t init(uvwasi_size_t argc, const char** argv) noexcept final
     {
+        if (m_inited)
+            uvwasi_destroy(&m_state);
+
         // Initialisation settings.
         // TODO: Make const after https://github.com/nodejs/uvwasi/pull/155 is merged.
         uvwasi_options_t options = {
@@ -27,7 +37,9 @@ public:
             0, 1, 2,
             nullptr,  // NOTE: no special allocator
         };
-        return uvwasi_init(&m_state, &options);
+        const auto ret = uvwasi_init(&m_state, &options);
+        m_inited = (ret == UVWASI_ESUCCESS);
+        return ret;
     }
 
     uvwasi_errno_t proc_exit(uvwasi_exitcode_t exit_code) noexcept final
