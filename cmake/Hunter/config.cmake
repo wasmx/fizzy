1eb82a208c56f6cc62811a976f9ace288bf2e8af
Paweł Bylica <chfast@gmail.com>
2021-12-20 12:47:00 +0100

    cmake: Upgrade dependencies

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,8 +1,18 @@
 # Hunter local configuration.
 
 hunter_config(
+    GTest
+    VERSION 1.11.0
+    URL https://github.com/google/googletest/archive/release-1.11.0.tar.gz
+    SHA1 7b100bb68db8df1060e178c495f3cbe941c9b058
+    CMAKE_ARGS
+    HUNTER_INSTALL_LICENSE_FILES=LICENSE
+    gtest_force_shared_crt=TRUE
+)
+
+hunter_config(
     benchmark
-    VERSION 1.5.3
-    URL https://github.com/google/benchmark/archive/v1.5.3.tar.gz
-    SHA1 32655d8796e708439ac5d4de8aa31f00d9dbda3b
+    VERSION 1.6.0
+    URL https://github.com/google/benchmark/archive/v1.6.0.tar.gz
+    SHA1 c4d1a9135e779c5507015ccc8c428cb4aca69cef
 )
