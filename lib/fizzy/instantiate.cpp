2727c1b5337d9c91ff5ee4d496e4449b441c22ce
Andrei Maiboroda <andrei@ethereum.org>
2021-12-27 13:28:03 +0100

    Fix overflow when matching imported memory of max size (4Gb)

--- lib/fizzy/instantiate.cpp
+++ lib/fizzy/instantiate.cpp
@@ -121,7 +121,8 @@ void match_imported_memories(const std::vector<Memory>& module_imported_memories
 
         const auto min = imported_memories[0].limits.min;
         const auto& max = imported_memories[0].limits.max;
-        if (size < min * PageSize || (max.has_value() && size > *max * PageSize))
+        if (size < uint64_t{min} * PageSize ||
+            (max.has_value() && size > uint64_t{*max} * PageSize))
             throw instantiate_error{"provided imported memory doesn't fit provided limits"};
     }
 }
